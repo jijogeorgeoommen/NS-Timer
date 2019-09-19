8674d9cdd36f1413199c78eb8a62906cc1285533
jijogeorgeoommen <jijogeorgeoommen@gmail.com>
2019-09-19 07:54:13 +0530

    initial commit

--- NS Timer/ViewController.swift
+++ NS Timer/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timer = Timer()
+    var count = 0
 
+    @IBOutlet weak var dIsplay: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func sTart(_ sender: Any) {
+        timer.invalidate()
+        timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(timeraction), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func sTop(_ sender: Any) {
+        timer.invalidate()
+        dIsplay.text = "0"
+        count = 0
+    }
+    
+    @objc func timeraction(){
+        
+        count = count + 1
+        dIsplay.text = "\(count)"
+        
+    }
 }
 
